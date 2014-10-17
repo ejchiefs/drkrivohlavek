69df2a28286e9a8efac2c51ca1a257ce794236f3
ejohnson <ejchiefs@eric-johnsons-macbook-pro.local>
2014-10-17 00:31:35 -0500

    feat(homepage): build home page

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,14 +11,19 @@
 <?php echo css('assets/styles/bootstrap-responsive.css'); ?>
 <?php echo css('assets/styles/bootstrap.css'); ?>
 <?php echo css('assets/styles/styles.css'); ?>
+<?php echo css('assets/js/fancybox/source/jquery.fancybox.css?v=2.1.5'); ?>
 <?php echo css('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'); ?>
 
-<?php echo css('assets/js/bootstrap.js'); ?>
+<?php echo js('//code.jquery.com/jquery-latest.min.js'); ?>
+<?php echo js('assets/js/bootstrap.js'); ?>
+<?php echo js('assets/js/scripts.js'); ?>
+<?php echo js('assets/js/fancybox/source/jquery.fancybox.pack.js?v=2.1.5'); ?>
 
 </head>
 
 <body>
 
+<?php snippet('appointment'); ?>
 <?php snippet('facebook'); ?>
 
 <header>
@@ -26,7 +31,7 @@
 		<a href="<?php echo url() ?>"><img src="<?php echo url('assets/images/logo.png') ?>" alt="<?php echo html($site->title()) ?>" class="logo" /></a>
 		<div class="contact">
 			<span class="inquiry">Have a Question? Call Now!</span><br/>
-			<span class="number">(402) 371-1170</span>
+			<span class="number"><?php echo html($site->phone()) ?></span>
 		</div>
 		<?php snippet('menu'); ?>
 	</div>
@@ -34,6 +39,18 @@
 
 <section id="banner">
 	<div class="banner">
-		<div class="curtains"></div>
+		<div class="curtains">
+			<div class="container">
+				<div class="row">
+					<div class="span5"></div>
+					<div class="span7">
+						<div class="customerQuote">
+							<p class="quotation">As always, my visit to your dental office was a pleasant experience. Dr. K. has been my dentist for all the 35 years he has been in practice so that should show that I think he is GREAT as well as his entire staff.</p>
+							<p class="quoter text-right">&mdash; LaDonna McMurty</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
