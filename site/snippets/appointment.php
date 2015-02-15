487f9154eb00cbb792f99dad5d3d277a31b1e892
ejohnson <ejohnson@rackspace.com>
2015-02-15 14:07:05 -0600

    master(feat): remove contact info from form

--- site/snippets/appointment.php
+++ site/snippets/appointment.php
@@ -34,107 +34,60 @@ if ($_POST) {
 
 <div style="display:none">
 	<div id="appointment">
-		<? /*< script type="text/javascript"  src="http://www.dentalratingsnetwork.com/myofficeratings/requestapptform?id=7PyCxA6gn0fOKu4">
-		</script> */ ?>
+
+		<?php /* < script type="text/javascript"  src="http://www.dentalratingsnetwork.com/myofficeratings/requestapptform?id=7PyCxA6gn0fOKu4"></script> */ ?>
 		
-		<FORM name="contactform" action="contact.php" method="POST" enctype="x-www-form-encoded">
+		<form name="contactform" action="contact.php" method="POST" enctype="x-www-form-encoded">
 		
-		<INPUT type="hidden" name="recipient" value="drk@drkrivohlavek.com">
-		<INPUT type="hidden" name="subject" value="Contact Response Form">
-		<INPUT type="hidden" name="title" value="Contact Response Form">
-		<INPUT type="hidden" name="redirect" value="http://www.drkrivohlavek.com/thankyou.php">
+		<input type="hidden" name="recipient" value="drk@drkrivohlavek.com">
+		<input type="hidden" name="subject" value="Contact Response Form">
+		<input type="hidden" name="title" value="Contact Response Form">
+		<input type="hidden" name="redirect" value="/">
 		
-		<table cellspacing="0" cellpadding="0" border="0" width="600">
+		<table cellspacing="0" cellpadding="0" border="0" width="400">
 		<tr>
-			<td width="600" height="300" align="center" valign="top">
-				<table cellspacing="0" cellpadding="0" border="0" width="600">
-				<tr>
-					<td width="300" align="left" valign="top">
-						<table cellspacing="0" cellpadding="0" border="0" width="300">
-						<tr>
-							<td width="120" height="25" align="left" valign="middle">Name</td>
-							<td width="180" height="25" align="left" valign="middle"><INPUT name="contact_name" type="text" size="25" class="contact"></td>
-						</tr>
-						<tr>
-							<td width="120" height="25" align="left" valign="middle">Address</td>
-							<td width="180" height="25" align="left" valign="middle"><INPUT name="contact_address" type="text" size="25" class="contact"></td>
-						</tr>
-						<tr>
-							<td width="120" height="25" align="left" valign="middle">City</td>
-							<td width="180" height="25" align="left" valign="middle"><INPUT name="contact_city" type="text" size="25" class="contact"></td>
-						</tr>
-						<tr>
-							<td width="120" height="25" align="left" valign="middle">State</td>
-							<td width="180" height="25" align="left" valign="middle"><INPUT name="contact_state" type="text" size="25" class="contact"></td>
-						</tr>
-						<tr>
-							<td width="120" height="25" align="left" valign="middle">Zip</td>
-							<td width="180" height="25" align="left" valign="middle"><INPUT name="contact_zip" type="text" size="25" class="contact"></td>
-						</tr>
-						<tr>
-							<td width="120" height="25" align="left" valign="middle">Phone Number</td>
-							<td width="180" height="25" align="left" valign="middle"><INPUT name="contact_phone" type="text" size="25" class="contact"></td>
-						</tr>
-						<tr>
-							<td width="120" height="25" align="left" valign="middle">Email Address</td>
-							<td width="180" height="25" align="left" valign="middle"><INPUT name="contact_email" type="text" size="25" class="contact"></td>
-						</tr>
-						<tr>
-							<td width="300" align="left" valign="top" colspan="2">
-								<br>Questions or Comments<br><br>
-								<TEXTAREA name="contact_comments" rows="4" cols="35" class="contact"></TEXTAREA>
-							</td>
-						</tr>
-						<tr>
-							<td width="300" height="15" colspan="2"></td>
-						</tr>
-						<tr>
-							<td width="300" align="left" valign="top" colspan="2">
-								<INPUT type="reset" name="reset" value="RESET" class="contact">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-								<INPUT type="submit" name="submit" value="SUBMIT" class="contact">
-							</td>
-						</tr>
-						</table>
-					</td>
-					<td width="300" align="left" valign="top">
-						<table cellspacing="0" cellpadding="0" border="0" width="300">
-						<tr>
-							<td width="300" height="215"></td>
-						</tr>
-						<tr>
-							<td width="300" align="center" valign="top">
-								<b>Dr. Bradley J. Krivohlavek, D.D.S.,P.C.</b><br>
-								2501 Lakeridge Drive, Suite 102<br>
-								Norfolk, NE 68701<br>
-								402.371.1170<br><br>
-								<b>Office Hours</b>
-							</td>
-						</tr>
-						</table>
-						
-						<table cellspacing="0" cellpadding="0" border="0" width="300">
-						<tr>
-							<td width="145" align="right" valign="top">
-								Mon,Wed,Thurs:<br>
-								Tuesday:<br>
-								Friday:
-							</td>
-							<td width="10"></td>
-							<td width="145" align="left" valign="top">
-								8-12 and 1-5<br>
-								7-1<br>
-								7-12
-							</td>
-						</tr>
-						</table>
-					</td>
-				</tr>
-				</table>
+			<td width="220" height="25" align="left" valign="middle">Name</td>
+			<td width="180" height="25" align="left" valign="middle"><input name="contact_name" type="text" size="25" class="contact" /></td>
+		</tr>
+		<tr>
+			<td width="220" height="25" align="left" valign="middle">Address</td>
+			<td width="180" height="25" align="left" valign="middle"><input name="contact_address" type="text" size="25" class="contact" /></td>
+		</tr>
+		<tr>
+			<td width="220" height="25" align="left" valign="middle">City</td>
+			<td width="180" height="25" align="left" valign="middle"><input name="contact_city" type="text" size="25" class="contact" /></td>
+		</tr>
+		<tr>
+			<td width="220" height="25" align="left" valign="middle">State</td>
+			<td width="180" height="25" align="left" valign="middle"><input name="contact_state" type="text" size="25" class="contact" /></td>
+		</tr>
+		<tr>
+			<td width="220" height="25" align="left" valign="middle">Zip</td>
+			<td width="180" height="25" align="left" valign="middle"><input name="contact_zip" type="text" size="25" class="contact" /></td>
+		</tr>
+		<tr>
+			<td width="220" height="25" align="left" valign="middle">Phone Number</td>
+			<td width="180" height="25" align="left" valign="middle"><input name="contact_phone" type="text" size="25" class="contact" /></td>
+		</tr>
+		<tr>
+			<td width="220" height="25" align="left" valign="middle">Email Address</td>
+			<td width="180" height="25" align="left" valign="middle"><input name="contact_email" type="text" size="25" class="contact" /></td>
+		</tr>
+		<tr>
+			<td width="400" align="left" valign="top" colspan="2">
+				<br>Questions or Comments<br><br/>
+				<textarea name="contact_comments" rows="4" cols="35" class="contact"></textarea>
 			</td>
+		</tr>
+		<tr>
+			<td height="15" colspan="2"></td>
+		</tr>
+		<tr>
+			<td align="left" valign="top" colspan="2"><input type="submit" name="submit" value="SUBMIT" class="contact" /></td>
 		</tr>
 		</table>
 		
-		</FORM>
+		</form>
 		
 	</div>
 </div>
